79bccd912660551fcd9444b32c4cac7001d2ca62
Tae Jeong Kim <oganesson12@hufs.ac.kr>
2025-01-10 14:52:17 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 88.90 ms, Memory: 27.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,17 @@
+# 스택/큐는 사용 안함
+
+def solution(arr):
+    answer = []
+    # answer 배열에 arr 첫번째 값 삽입
+    answer.append(arr[0])
+    # arr 배열 하나씩 돌면서
+    # answer 마지막 값과 같은지 판단
+    for i in range(len(arr)):
+        # 같으면 패스
+        if arr[i] == answer[-1]:
+            continue
+        # 다르면 answer에 추가
+        else:
+            answer.append(arr[i])
+        
+    return answer
